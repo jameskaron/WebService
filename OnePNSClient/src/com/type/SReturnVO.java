ec08adf880e325481a33afc3ac7c1f296f422c03
jameskaron <jameskaron@126.com>
2015-10-23 16:47:47 +0800

    20151023

---
+++ OnePNSClient/src/com/type/SReturnVO.java
@@ -0,0 +1,79 @@
+
+package com.type;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>SReturnVO complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType name="SReturnVO">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="resultCode" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="resultDesc" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "SReturnVO", propOrder = {
+    "resultCode",
+    "resultDesc"
+})
+public class SReturnVO {
+
+    protected int resultCode;
+    protected String resultDesc;
+
+    /**
+     * 获取resultCode属性的值。
+     * 
+     */
+    public int getResultCode() {
+        return resultCode;
+    }
+
+    /**
+     * 设置resultCode属性的值。
+     * 
+     */
+    public void setResultCode(int value) {
+        this.resultCode = value;
+    }
+
+    /**
+     * 获取resultDesc属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getResultDesc() {
+        return resultDesc;
+    }
+
+    /**
+     * 设置resultDesc属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setResultDesc(String value) {
+        this.resultDesc = value;
+    }
+
+}
---
+++ OnePNSServer/src/com/type/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "rm:type")
+package com.type;
